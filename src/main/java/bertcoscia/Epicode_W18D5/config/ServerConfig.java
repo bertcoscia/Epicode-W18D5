97889f979b73adce0cb9ac8a5cb1e208e7240717
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-09-13 12:22:20 +0200

    adds ServerConfig

---
+++ src/main/java/bertcoscia/Epicode_W18D5/config/ServerConfig.java
@@ -0,0 +1,24 @@
+package bertcoscia.Epicode_W18D5.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerConfig {
+
+    @Bean
+    public Cloudinary imageUploader(@Value("${cloudinary.name}") String name,
+                                    @Value("${cloudinary.key}") String key,
+                                    @Value("${cloudinary.secret}") String secret) {
+        Map<String, String> configuration = new HashMap<>();
+        configuration.put("cloud_name", name);
+        configuration.put("api_key", key);
+        configuration.put("api_secret", secret);
+        return new Cloudinary(configuration);
+    }
+
+}
